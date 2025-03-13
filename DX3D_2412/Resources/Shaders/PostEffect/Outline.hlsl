4bd7643701ca03f27686d28eceff7ef4bdf9acb4
Kim, Hyunbok <asd2590@naver.com>
2025-03-13 12:02:11 +0900

    Update

---
+++ DX3D_2412/Resources/Shaders/PostEffect/Outline.hlsl
@@ -0,0 +1,32 @@
+#include "../VertexHeader.hlsli"
+#include "../PixelHeader.hlsli"
+
+struct PixelInput
+{
+    float4 pos : SV_POSITION;
+    float2 uv : UV;
+};
+
+PixelInput VS(VertexUV input)// : SV_POSITION//SV->RS
+{
+    PixelInput output;
+    output.pos = mul(input.pos, world);
+    output.pos = mul(output.pos, view);
+    output.pos = mul(output.pos, projection);
+    output.uv = input.uv;
+    
+    return output;
+}
+
+cbuffer ValueBuffer : register(b10)
+{
+    float scale;
+    float2 imageSize;
+}
+
+float4 PS(PixelInput input) : SV_TARGET //->SV->OM
+{	
+    float4 baseColor = diffuseMap.Sample(samp, input.uv);
+      
+    return float4(1, 0, 0, baseColor.a);
+}
--- DX3D_2412/Scenes/OutlineScene.cpp
+++ DX3D_2412/Scenes/OutlineScene.cpp
@@ -10,7 +10,7 @@ OutlineScene::OutlineScene()
 
 	Texture* texture = Texture::Add(L"Target", renderTarget->GetSRV());
 	quad = new Quad(Vector3(SCREEN_WIDTH, SCREEN_HEIGHT));
-	quad->GetMaterial()->SetShader(L"PostEffect/Grayscale.hlsl");
+	quad->GetMaterial()->SetShader(L"PostEffect/Outline.hlsl");
 	quad->GetMaterial()->SetDiffuseMap(texture);
 	quad->SetLocalPosition(CENTER);
 	quad->UpdateWorld();
@@ -33,6 +33,8 @@ void OutlineScene::Update()
 
 void OutlineScene::PreRender()
 {
+	renderTarget->Set(depthStencil, Float4(0, 0, 0, 0));
+	testModel->Render();
 }
 
 void OutlineScene::Render()
@@ -41,7 +43,8 @@ void OutlineScene::Render()
 }
 
 void OutlineScene::PostRender()
-{
+{	
+	quad->Render();
 }
 
 void OutlineScene::GUIRender()
